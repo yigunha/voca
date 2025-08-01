64bca3c9f1994673c515b5355e36f8e07a692718
YiGunHa <isinegajok@naver.com>
2025-08-01 23:30:19 +0900

    Add files via upload

---
+++ Words/idn-4A01.js
@@ -0,0 +1,23 @@
+const words = [
+"세계 일주",
+"해외 봉사 활동",
+"캠핑카 여행",
+"패러글라이딩",
+"개인 방송 채널 만들기",
+"내가 살 집 짓기",
+"마라톤에서 끝까지 달리기",
+"세계의 맛있는 음식 다 먹어 보기",
+"외국에서 한 달 살기"
+];
+
+const englishTranslations = [
+"keliling dunia",
+"kegiatan sukarela di luar negeri",
+"berwisata dengan mobil karavan",
+"paralayang",
+"membuat saluran siaran pribadi",
+"membangun rumah untuk ditinggali",
+"menyelesaikan maraton sampai akhir",
+"mencicipi semua makanan lezat di dunia",
+"tinggal di luar negeri selama sebulan"
+];
