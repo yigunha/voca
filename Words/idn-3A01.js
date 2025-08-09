c16de322f63f1ebe1f477a4980ad63d5198c0239
YiGunHa <isinegajok@naver.com>
2025-08-09 21:31:44 +0900

    Add files via upload

---
+++ Words/idn-3A01.js
@@ -0,0 +1,25 @@
+const words = [
+  "오랜만이야",
+  "이곳저곳 다니다",
+  "한가하게 지내다",
+  "특별한 일 없이 지내다",
+  "웬일이야",
+  "여기저기 다니다",
+  "정신없이 지내다",
+  "이게 얼마 만이야",
+  "이런저런 이야기를 하다",
+  "그저 그렇게 지내다"
+];
+
+const englishTranslations = [
+  "Sudah lama tidak bertemu",
+  "pergi ke sana kemari",
+  "hidup santai",
+  "tidak ada yang istimewa",
+  "Ada apa?",
+  "pergi ke sana kemari",
+  "hidup sibuk sekali",
+  "Sudah berapa lama ini?",
+  "bercerita tentang ini dan itu",
+  "hanya begitu-begitu saja"
+];
