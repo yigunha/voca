648e687bc672ff0a0c87469503ab35e72c13cfe6
YiGunHa <isinegajok@naver.com>
2025-08-09 20:23:53 +0900

    Add files via upload

---
+++ Words/idn-4B01.js
@@ -0,0 +1,33 @@
+const words = [
+  "외향적이다",
+  "내성적이다",
+  "적극적이다",
+  "소극적이다",
+  "자신감이 있다",
+  "자신감이 없다",
+  "성실하다",
+  "불성실하다",
+  "책임감이 있다",
+  "책임감이 없다",
+  "급하다",
+  "느긋하다",
+  "유머 감각이 있다",
+  "유머 감각이 없다"
+];
+
+const indonesianTranslations = [
+  "Ekstrovert",
+  "Introvert",
+  "Aktif",
+  "Pasif",
+  "Percaya diri",
+  "Tidak percaya diri",
+  "Rajin",
+  "Malas",
+  "Bertanggung jawab",
+  "Tidak bertanggung jawab",
+  "Terburu-buru",
+  "Santai",
+  "Punya selera humor",
+  "Tidak punya selera humor"
+];
