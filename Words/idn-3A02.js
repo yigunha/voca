c54cefc1b7b29fdef6549bb4cec8e49b5e8959f9
YiGunHa <isinegajok@naver.com>
2025-08-19 17:05:24 +0900

    Add files via upload

---
+++ Words/idn-3A02.js
@@ -0,0 +1,16 @@
+const words = [
+    "입학을 하다", "졸업을 하다", "일자리를 구하다",
+    "일자리를 찾다", "일을 맡다", "업무를 맡다",
+    "운전면허를 따다", "장학금을 받다", "회사에 지원하다",
+    "승진하다", "청혼을 하다", "청혼을 받다",
+    "휴학하다", "회사에서 근무하다", "일을 그만두다",
+    "아이를 낳다"
+];
+const englishTranslations = [
+"masuk sekolah/kuliah", "lulus", "mencari pekerjaan",
+"mendapat pekerjaan", "mengambil pekerjaan", "mengambil pekerjaan",
+"mendapat SIM (Surat Izin Mengemudi)", "mendapat beasiswa", "melamar pekerjaan di perusahaan",
+"naik jabatan/dipromosikan", "melamar", "diterima lamarannya",
+"mengambil cuti akademik", "bekerja di perusahaan", "mengundurkan diri",
+"melahirkan anak"
+];
