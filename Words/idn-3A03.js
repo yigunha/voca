a871bfada7316fe058e0f11e371549bab6c5ac47
YiGunHa <isinegajok@naver.com>
2025-09-03 13:43:28 +0700

    Add files via upload

---
+++ Words/idn-3A03.js
@@ -0,0 +1,16 @@
+const words = [
+    "입학을 하다", "졸업을 하다", "일자리를 구하다",
+    "일자리를 찾다", "일을 맡다", "업무를 맡다",
+    "운전면허를 따다", "장학금을 받다", "회사에 지원하다",
+    "승진하다", "청혼을 하다", "청혼을 받다",
+    "휴학하다", "회사에서 근무하다", "일을 그만두다",
+    "아이를 낳다"
+];
+const englishTranslations = [
+"Properti", "Sewa bulanan", "Jasa pindahan",
+"Uang jaminan", "Kontrak", "Hari pindahan", 
+"Mengepak barang pindahan", "Menata barang pindahan", "Dekat dengan pasar", 
+"Memindahkan barang pindahan", "Mengundang ke rumah", "Transportasi mudah", 
+"Membongkar barang pindahan", "Mengadakan pesta syukuran rumah baru", "Memindahkan barang pindahan", 
+"Lingkungan sekitarnya tenang"
+];
