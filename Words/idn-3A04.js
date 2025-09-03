f81528e2f56426b467790e7311e4d309d979f249
YiGunHa <isinegajok@naver.com>
2025-09-03 14:38:00 +0700

    Add files via upload

---
+++ Words/idn-3A04.js
@@ -0,0 +1,18 @@
+const words = [
+	"청소기", "세탁기", "쓰레기통", 
+	"걸레", "대걸레", "주방 세제", 
+	"세탁 세제", "빗자루", "쓰레받기", 
+	"고무장갑", "청소기를 돌리다", "세탁기를 돌리다", 
+	"바닥을 쓸다", "바닥을 닦다", "쓰레기를 버리다", 
+	"설거지를 하다", "빨래를 널다", "쓰레기통을 비우다", 
+	"손빨래를 하다", "걸레질을 하다", "먼지를 털다"
+];
+const englishTranslations = [
+	"Penyedot debu", "Mesin cuci", "Tempat sampah", 
+	"Kain pel", "Pel", "Sabun cuci piring",
+	"Deterjen", "Sapu", "Pengki", 
+	"Sarung tangan karet", "Menyalakan penyedot debu", "Menyalakan mesin cuci",
+	"Menyapu lantai", "Mengepel lantai", "Membuang sampah",
+	"Mencuci piring", "Menjemur pakaian", "Mengosongkan tempat sampah", 
+	"Mencuci pakaian dengan tangan", "Mengepel", "Mengibas debu"
+];
