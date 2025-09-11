c6664fb2aaa39c57868b07856d4afc7c1c0839b4
YiGunHa <isinegajok@naver.com>
2025-09-11 16:42:30 +0700

    Add files via upload

---
+++ idn-4A04.js
@@ -0,0 +1,14 @@
+const words = [
+	"죽다", "가뭄", "홍수",
+	"폭발", "폭우", "폭설",
+	"산사태", "전염병", "피해가 발생하다",
+	"다치다", "실종되다", "사망하다",
+	"부상자", "사망자", "부상을 당하다"
+];
+const englishTranslations = [
+	"Mati", "Kekeringan", "Banjir",
+	"Ledakan", "Hujan deras", "Salju lebat",
+	"Tanah longsor", "Wabah penyakit menular", "Terjadi kerusakan",
+	"Terluka", "Hilang", "Meninggal",
+	"Korban luka-luka", "Korban jiwa", "Mengalami cedera"
+];
