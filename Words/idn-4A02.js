c671ea70be4049548789644f268d1160aa648a75
YiGunHa <isinegajok@naver.com>
2025-09-11 16:47:16 +0700

    Add files via upload

---
+++ Words/idn-4A02.js
@@ -0,0 +1,12 @@
+const words = [
+	"여유롭다", "이국적이다", "낭만적이다",
+	"현대적이다", "활기가 넘치다", "색다르다",
+	"신기하다", "역사가 깊다", "전망이 좋다",
+	"촬영지로 유명하다"
+];
+const englishTranslations = [
+	"Santai", "Eksotis", "Romantis",
+	"Modern", "Penuh semangat", "Unik",
+	"Menakjubkan", "Bersejarah", "Pemandangannya bagus",
+	"Terkenal sebagai lokasi syuting"
+];
