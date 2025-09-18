d7920dc8312f74751f6f03439661f31304b32b2d
YiGunHa <isinegajok@naver.com>
2025-09-18 17:52:48 +0700

    Add files via upload

---
+++ data-parentheses/data1.js
@@ -0,0 +1,12 @@
+window.questionsData = [
+    { 
+        description: "마음에 들지 않거나 사이즈가 맞지 않아서 다른 물건으로 바꿔요.",
+        question: "마음에 들지 않거나 사이즈가 맞지 않아서 다른 물건으로 바꿔요. : (   )", 
+        answers: [["교환", "교환하다", "바꾸기"]]
+    },
+    { 
+        image: "11,12,13",
+        question: "(   )과 (   )입니다.",
+        answers: [["지구", "Earth", "earth"], ["태양", "해", "sun", "Sun"]]
+    }, 
+];
---
+++ data-parentheses/idn-parentheses_3A-ik.js
@@ -0,0 +1,72 @@
+window.questionsData = [
+  {
+    description: "Ukuran baju atau sepatu pas di badan.",
+    question: "옷이나 신발의 크기가 몸에 잘 맞아요. : (   )",
+    answers: [["사이즈가 딱 맞다", "사이즈가 맞다"]]
+  },
+  {
+    description: "Ukuran baju atau sepatu tidak pas di badan.",
+    question: "옷이나 신발의 크기가 몸에 안 맞아요. : (   )",
+    answers: [["사이즈가 안 맞다"]]
+  },
+  {
+    description: "Barang rusak jadi tidak bisa dipakai lagi.",
+    question: "물건에 문제가 생겨서 사용할 수 없게 되었어요. : (   )",
+    answers: [["망가지다", "고장나다"]]
+  },
+  {
+    description: "Membayar harga barang.",
+    question: "물건값을 내요. : (   )",
+    answers: [["결제하다", "결제"]]
+  },
+  {
+    description: "Desain barangnya saya suka.",
+    question: "물건의 모양이 마음에 들어요. : (   )",
+    answers: [["디자인이 마음에 들다"]]
+  },
+  {
+    description: "Ada noda kotor di barangnya.",
+    question: "물건에 지저분한 자국이 있어요. : (   )",
+    answers: [["얼룩이 있다","얼룩이 묻었다" ]]
+  },
+  {
+    description: "Menukar barang karena tidak suka atau ukurannya tidak pas.",
+    question: "마음에 들지 않거나 사이즈가 맞지 않아서 다른 물건으로 바꿔요. : (   )",
+    answers: [["교환하다", "교환"]]
+  },
+  {
+    description: "Membayar harga barang dengan kartu kredit.",
+    question: "카드로 물건값을 내요 : (   )",
+    answers: [["카드로 계산하다","카드로 결제하다"]]
+  },
+  {
+    description: "Membayar harga barang dengan uang tunai (uang kertas, koin).",
+    question: "돈으로 물건값을 내요. : (   )",
+    answers: [["현금으로 계산하다","현금으로로 결제하다"]]
+  },
+  {
+    description: "Baju atau perhiasan cocok dan bagus kalau dipakai.",
+    question: "옷이나 장신구가 나에게 잘 맞아서 보기 좋아요. : (   )",
+    answers: [["어울리다"]]
+  },
+  {
+    description: "Baju atau perhiasan tidak cocok dan tidak bagus kalau dipakai.",
+    question: "옷이나 장신구가 나에게 잘 안 맞아서 보기 안 좋아요. : (   )",
+    answers: [["안 어울리다"]]
+  },
+  {
+    description: "Hiasan yang menempel di barang lepas.",
+    question: "물건에 붙어 있던 꾸미는 부분이 떨어졌어요. : (   )",
+    answers: [["장식이 떨어지다"]]
+  },
+  {
+    description: "Tali yang ada di sepatu atau tas lepas.",
+    question: "신발이나 가방에 달린 끈이 떨어졌어요. : (   )",
+    answers: [["끈이 떨어지다"]]
+  },
+  {
+    description: "Mengembalikan barang dan mendapatkan uangnya kembali.",
+    question: "물건을 다시 주고 물건값을 돌려받아요. : (   )",
+    answers: [["환불하다", "환불"]]
+  }
+];
