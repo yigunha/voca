d96e3f24c8c2de54ada3b47a9afd59d65eb13a75
YiGunHa <isinegajok@naver.com>
2025-09-20 09:11:48 +0700

    Add files via upload

---
+++ data-parentheses/data3.js
@@ -0,0 +1,10 @@
+window.questionsData = [
+    { 
+	image: "3A-딱 맞아요,3A-딱 맞아요,3A-딱 맞아요,3A-새 옷,13",
+        description: "마음에 들지 않거나 사이즈가 맞지 않아서 다른 물건으로 바꿔요.",
+
+        question: "(   ).", 
+
+        answers: [["새 옷을 입어보니까 딱 맞아요"]]
+    }, 
+];
